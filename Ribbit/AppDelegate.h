a7eb1cfbbbd463832ae761d4e97169fc2b069564
P <pashapal@gmail.com>
2014-02-04 11:03:19 -0600

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by P on 2/4/14.
+//  Copyright (c) 2014 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
