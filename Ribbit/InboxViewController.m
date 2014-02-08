824f4799aa8300f15ef741f18ef1d3e99d201a5d
P <pashapal@gmail.com>
2014-02-08 12:31:09 -0600

    Added the ability to embed videos in the inbox. Used the Media framework and MPMoviePlayer.

--- Ribbit/InboxViewController.m
+++ Ribbit/InboxViewController.m
@@ -18,6 +18,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.moviePlayer = [[MPMoviePlayerController alloc] init];
 
     PFUser *currentUser = [PFUser currentUser];
     if (currentUser) {
@@ -91,7 +93,16 @@
         [self performSegueWithIdentifier:@"showImage" sender:self];
     }
     else {
-    
+        // file type is video
+        PFFile *videoFile = [self.selectedMessage objectForKey:@"file"];
+        NSURL *fileUrl = [NSURL URLWithString:videoFile.url];
+        self.moviePlayer.contentURL = fileUrl;
+        [self.moviePlayer prepareToPlay];
+        [self.moviePlayer thumbnailImageAtTime:0 timeOption:MPMovieTimeOptionNearestKeyFrame];
+        
+        // Add it to the view controller so we can see it
+        [self.view addSubview:self.moviePlayer.view];
+        [self.moviePlayer setFullscreen:YES animated:YES];
     }
     
 }
